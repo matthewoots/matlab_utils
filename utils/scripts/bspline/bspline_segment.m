6aa06b902d92d516eeef573aa6d2b2e3a64cd0e9
matthewoots <matthewoots@gmail.com>
2021-11-02 22:29:05 +0800

    Fixed the spline segment and added more notations to provide better understanding, included jerk in visualization

--- utils/scripts/bspline/bspline_segment.m
+++ utils/scripts/bspline/bspline_segment.m
@@ -28,41 +28,52 @@ clear all
 funct_path = '../../functions/bspline_utils';
 addpath(funct_path);
 
-order = 3;
+order = 4;
 k = order + 1;
-% 5th order was used in the paper
+
 M = getM(order);
-u = zeros(1,k); du = zeros(1,k); ddu = zeros(1,k);
+u = zeros(1,k); du = zeros(1,k); ddu = zeros(1,k); dddu = zeros(1,k);
 p = zeros(k,1);
+
+% p = degree
+% n = cp - 1
+% knots = m + 1
+% basics is m = n + p + 1
+% Open B-splines are [knot(p), knot(m-p)]
+% * Clamping only happens for the knots not for the control points!
 
 %% User inputs
 timespan = [0,25];
-r = rand(1,30);
-range = 12;
-P = range * r;
+r = rand(1,35);
+numrange = 12; % range for evaluation
+P = numrange * r; % mutltiple the random [0-1] by the number range
+n = numel(r) - 1;
 
 % How much to segment a knotspan (internal evaluation in a spline segment)
-kn_seg = 20;
+buffer = zeros(1,order + 1);
+knots_total = n + order + 1 + 1;
 % Range of index to evaluate according
-range = [7:15]; 
+range = [order:numel(P)-order]; 
 
 
 %% Main code
-t = linspace(timespan(1), timespan(2), numel(P));
+% I may have made the code abit different by not using knots = m + 1 which
+% means using numel(P) rather than knots_total to segment the time horizon
+t = linspace(timespan(1), timespan(2), numel(P)); % knots
+% t = linspace(timespan(1), timespan(2), knots_total); % knots
+dt = (timespan(2) - timespan(1)) / (numel(P)-1); % span of 1 knot
+kn_seg = 15; % Division of 1 span or 1 segment
 
 for l = 1:numel(range)
-    % Matlab notation have to be changed since 0 is 1 in Matlab
-    % Because idx 1 should represent 0
-    idx = range(l)-1;
-    nxt_idx = idx +1; 
+    idx = range(l) - order;
+    nxt_idx = idx + 1; 
     lpos = zeros(1,kn_seg-1);
     lvel = zeros(1,kn_seg-1);
     lacc = zeros(1,kn_seg-1);
     
     span = linspace(idx, nxt_idx, kn_seg);
-    dt = (timespan(2) - timespan(1))/(numel(P)-1);
-
-    if idx - order <= 0
+    
+    if idx < 0
         error('idx is below suggested idx for control points');
     end 
     
@@ -70,29 +81,34 @@ for l = 1:numel(range)
         error('idx is out of bounds compared to control points');
     end
     
+    tic
+    % numel(span)-1 becasue we are considering [0,1) hence not including 1
     for m = 1:numel(span)-1
-        
+
         % Using convention from (1) to get u
-        time = span(m) * dt;
-        u_t = (time - t(range(l)))/(t(range(l)+1) - t(range(l)));
+        time = span(m); % current time in index form, of course we dont like to play with conversion
+        u_t = (time - idx)/((idx+1) - idx); % using index is the same as using time since u_t is a factor
         % Save the time constant (0 to 1) and the actual time of the point
         timeConst(m) = u_t;
         timeActual(m) = span(m) * dt;
 
         % p have several conventions according to SO many papers but i
-        % would use the convention under (1)
+        % would use the convention under (1) and it is the correct one
+        % etc if order is 5
         % (1) p = [P(idx-5) P(idx-4) P(idx-3) P(idx-2) P(idx-1) P(idx)]';
-        % (2) p = [P(idx-2) P(idx-1) P(idx) P(idx+1) P(idx+2) P(idx+3)]';
         
         % Make the u, du, ddu and p matrix
         for n = 1:k
             u(n) = u_t^(n-1);
-            p(n) = P(idx - order + (n-1));
+            p(n) = P(idx + (n-1) + 1); % we add a +1 here for matlab notation
             if n >= 2
                 du(n) = (n-1) * u_t^(n-2);
             end
             if n >= 3
                 ddu(n) = (n-1) * (n-2) * u_t^(n-3);
+            end
+            if n >= 4
+                dddu(n) = (n-1) * (n-2) * (n-3) * u_t^(n-4);
             end
         end
         
@@ -102,6 +118,7 @@ for l = 1:numel(range)
         lpos(m) = u * M * p;
         lvel(m) = inv_dt * du * M * p;
         lacc(m) = inv_dt^2 * ddu * M * p;
+        ljrk(m) = inv_dt^3 * dddu * M * p;
     end
     
     % Add the segment to the plot array
@@ -109,39 +126,55 @@ for l = 1:numel(range)
         pos = [pos lpos];
         vel = [vel lvel];
         acc = [acc lacc];
+        jrk = [jrk ljrk];
         tt = [tt timeActual];
-        tc = [tc timeConst];
+        % tc = [tc timeConst];
     else
         pos = lpos;
         vel = lvel;
         acc = lacc;
+        jrk = ljrk;
         tt = timeActual;
-        tc = timeConst;
+        % tc = timeConst;
     end
 end
+
+fprintf("Time Elapsed %f\n",toc);
 
 %% Plot 
 % For plotting purposes
 
 figure
-subplot(3,1,1)
+subplot(4,1,1)
 hold on
 for i = 1:numel(range)
-    plot(t(range(i)),P(range(i)),'o');
+    % t(i) should be empty and the control point matching with the time
+    % should be +1, that is why t(i+1)
+    plot(t(i+1),P(range(i)),'o');
 end
 plot(tt,pos,'x')
+title("Position")
 hold off
 grid on
 
-subplot(3,1,2)
+subplot(4,1,2)
 hold on
 plot(tt,vel,'x')
+title("Velocity")
 hold off
 grid on
 
-subplot(3,1,3)
+subplot(4,1,3)
 hold on
 plot(tt,acc,'x')
+title("Acceleration")
+hold off
+grid on
+
+subplot(4,1,4)
+hold on
+plot(tt,jrk,'x')
+title("Jerk")
 hold off
 grid on
 
